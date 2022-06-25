25ea4247b08e79e2812911603f560da22e5e6dce
paantya <paantya@gmail.com>
2022-06-25 03:11:46 +0200

    [add] config

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "<TOKEN>"
+
+CHAT_ID = <channel_id>
+CHAT_ID_LOG = <channel_log_id>
+CHAT_TD_LOG = <channel_log_id>
+
+NO_PHOTO = 'https://www.proskidku.ru/local/templates/.default/components/bitrix/catalog.element/proskidku/images/no_photo.png'
+
+msg_info = <msg.json()>
+
+msg_info_log = <msg_log.json()>
